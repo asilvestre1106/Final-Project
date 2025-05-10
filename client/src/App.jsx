9604651808924ed0728cbed0f21c8bf4e0b56c0d
Andre Silvestre <goodsdoggn@gmail.com>
2025-05-10 20:53:29 +0800

    lots of changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,32 @@
 import { useState } from 'react'
+import {Routes, Route} from "react-router-dom";
 import './App.css'
+import Post from './Post.jsx'
+import Header from './Header.jsx'
 
 function App() {
-
   return (
     <>
-    <main>
-      <header>
-        <a href="" className="logo">SilvestreBlog</a>
-        <nav>
-          <a href="" className="login">Login</a>
-          <a href="" className="register">Register</a>
-        </nav>
-      </header>
+      <Routes>
+        <Route index element ={
+        <main>
+          <Header/>
+          <Post />
+          <Post />
+          <Post />
+        </main>
+        } />
+        <Route path = {'/login'} element = {
+        <main>
+          <Header/>
+          <div>Login page</div>
+        </main>
+        } />
+      </Routes>
       
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/07/app-store-ios-2022-b.jpg?resize=1098,617" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Epic Games and Spotify test Apple’s new App Store rules</h2>
-          <p className="info">
-            <a href="" className="author">Andre Silvestre</a>
-            <time>2025-10-06 8:27</time>
-          </p>
-          <p className="summary">Fortnite maker Epic Games and Spotify are testing Apple’s new App Store policies by submitting apps for review that would have previously never been allowed.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/07/app-store-ios-2022-b.jpg?resize=1098,617" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Epic Games and Spotify test Apple’s new App Store rules</h2>
-          <p className="info">
-            <a href="" className="author">Andre Silvestre</a>
-            <time>2025-10-06 8:27</time>
-          </p>
-          <p className="summary">Fortnite maker Epic Games and Spotify are testing Apple’s new App Store policies by submitting apps for review that would have previously never been allowed.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/07/app-store-ios-2022-b.jpg?resize=1098,617" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Epic Games and Spotify test Apple’s new App Store rules</h2>
-          <p className="info">
-            <a href="" className="author">Andre Silvestre</a>
-            <time>2025-10-06 8:27</time>
-          </p>
-          <p className="summary">Fortnite maker Epic Games and Spotify are testing Apple’s new App Store policies by submitting apps for review that would have previously never been allowed.</p>
-        </div>
-      </div>
-    
-      
-    </main>
     </>
+    
+    
   )
 }
 
---
+++ client/src/Post.jsx
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className="post">
+            <div className="image">
+                <img src="https://techcrunch.com/wp-content/uploads/2022/07/app-store-ios-2022-b.jpg?resize=1098,617" alt="" />
+            </div>
+            <div className="texts">
+                <h2>Epic Games and Spotify test Apple’s new App Store rules</h2>
+                <p className="info">
+                    <a href="" className="author">Andre Silvestre</a>
+                    <time>2025-10-06 8:27</time>
+                </p>
+                <p className="summary">Fortnite maker Epic Games and Spotify are testing Apple’s new App Store policies by submitting apps for review that would have previously never been allowed.</p>
+            </div>
+      </div>
+    ) 
+}
