a8df9b84544b3a6c1ab20a3ec077b215cb059f73
Andre Silvestre <goodsdoggn@gmail.com>
2025-05-11 15:13:57 +0800

    sadasd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,25 +3,16 @@ import {Routes, Route} from "react-router-dom";
 import './App.css'
 import Post from './Post.jsx'
 import Header from './Header.jsx'
+import Layout from './Layout.jsx';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index element ={
-        <main>
-          <Header/>
-          <Post />
-          <Post />
-          <Post />
-        </main>
-        } />
-        <Route path = {'/login'} element = {
-        <main>
-          <Header/>
-          <div>Login page</div>
-        </main>
-        } />
+        <Route path = "/" element = {<Layout/>}>
+          <Route index element ={<Post/>} />
+          <Route path = {'/login'} element = {<div>Login page</div>} />
+        </Route>
       </Routes>
       
     </>
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Header(){
     return (
         <header>
-        <a href="" className="logo">SilvestreBlog</a>
+        <Link to="/" className="logo">SilvestreBlog</Link>
         <nav>
             <Link to="/login" className="login">Login</Link>
             <Link to="/register" className="register">Register</Link>
