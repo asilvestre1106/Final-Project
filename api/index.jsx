fdb9186f9f2e7c55311152c117990580d46b5788
Andre Silvestre <goodsdoggn@gmail.com>
2025-05-12 11:16:04 +0800

    post

--- api/index.jsx
+++ api/index.jsx
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs'); //password hashing
 const app = express(); //express app
 const jwt = require('jsonwebtoken'); //token generation
 const cookieParser = require('cookie-parser'); //cookie parser
+const uploadMiddleware = require('multer'); //file upload
 
 const secret = 'aoisduoasifasoidfnaosi';
 const salt = bcrypt.genSaltSync(10);
@@ -17,7 +18,8 @@ app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:bXwPqvc35t2wwqG7@cluster0.zg4vgxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
-
+// gets the input from the form and saves it to the database
+// also hashes the password
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try{
@@ -31,6 +33,11 @@ app.post('/register', async (req, res) => {
   }
 });
 
+//checks if the user is already registered
+//if not, it errors out
+//if the user is registered, it checks if the password is correct 
+//if the password is correct, it generates a token and sends it to the client
+//if the password is incorrect, it errors out
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne ({username})
@@ -38,21 +45,33 @@ app.post('/login', async (req, res) => {
   if (passOk) {
     jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
       if(err) throw err;
-      res.cookie('token', token).json('ok');
+      res.cookie('token', token).json({
+        id: userDoc._id,
+        username,
+      });
     });
   } else {
     res.status(400).json('Wrong credentials');
   }
 });
 
+//checks if the user is logged in
+//if the user is logged in, it sends the user info to the client
 app.get('/profile', (req, res) => {
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, (err, info)=>{
     if(err) throw err;
     res.json(info);
   })
-  res.json(req.cookies);
 });
 
+//logs the user out by clearing the cookie
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), (req, res) => {
+  res.json(req.files);
+}); 
 
 app.listen(4000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,23 @@ import Layout from './Layout.jsx';
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import CreatePost from './pages/CreatePost.jsx';
+import { UserContextProvider } from './UserContext.jsx';
 
 function App() {
   return (
     <>
+    <UserContextProvider>
       <Routes>
         <Route path = "/" element = {<Layout/>}>
           <Route index element ={<HomePage/>} />
           <Route path = "/login" element = {<LoginPage/>} />
           <Route path = "/register" element = {<RegisterPage/>} />
+          <Route path = "/create" element = {<CreatePost />} />
         </Route>
       </Routes>
+    </UserContextProvider>
+      
       
     </>
     
