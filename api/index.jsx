7dc08fc0323fe42b8e463e1f0f7a6b70b45ddb01
Andre Silvestre <goodsdoggn@gmail.com>
2025-05-12 19:05:11 +0800

    edit post

--- api/index.jsx
+++ api/index.jsx
@@ -103,4 +103,11 @@ app.get('/post', async (req, res) => {
   );
 })
 
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await Post.findById(id)
+  .populate('author', ['username'])
+  res.json(postDoc);
+})
+
 app.listen(4000);
